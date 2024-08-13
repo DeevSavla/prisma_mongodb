1ed6ef2c31e4d0f42971576a727f0b7878f0301f
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2024-08-13 17:41:42 +0530

    add login and logout controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,74 @@
 //bring prisma and cookie 
 import {prisma} from '../prisma/index.js'
 import {cookieToken} from '../utils/cookieToken.js'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 //signup 
 const signUpController = async (req,res,next)=>{
     try {
-        const {name,email,password} = req.body
-        if(!name || !email || !password){
+        const {name,email} = req.body
+        if(!name || !email || !req.body.password){
             throw new Error('Please provide all fields')
         }
+        const salt = bcryptjs.genSaltSync(10)
+        const hashedPassword = bcryptjs.hashSync(req.body.password,salt)
+        const password = hashedPassword
+
         const user = await prisma.user.create({
             data:{
                 name,
                 email,
-                password,
+                password:password,
             }
         })
+        
         cookieToken(user,res)
+
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const loginController = async(req,res) =>{
+    try {
+        const {email,password} = req.body
+
+        if(!email || !password){
+            throw new Error('Please provide all fields')
+        }
+
+        const findUser = await prisma.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        if(!findUser){
+            throw new Error('User not found')
+        }
+
+        const isPasswordCorrect = bcryptjs.compare(password,findUser.password)
+        if(!isPasswordCorrect){
+            throw new Error('Password Incorrect')
+        }
+        findUser.password = undefined
+
+        cookieToken(findUser,res)
+
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const logoutController = async(req,res) => {
+    try {   
+        res.clearCookie('token')
+
+        res.status(200).json({
+            success:true,
+            message:'Logout Successfull'
+        })
     } catch (error) {
         throw new Error(error)
     }
@@ -24,4 +76,6 @@ const signUpController = async (req,res,next)=>{
 
 export {
     signUpController,
+    loginController,
+    logoutController,
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,11 @@
-import {signUpController} from '../controllers/user.controller.js'
+import {signUpController,loginController,logoutController} from '../controllers/user.controller.js'
 import express from 'express'
 
 const userRouter = express.Router()
 
 userRouter.post('/signup',signUpController)
+userRouter.post('/login',loginController)
+userRouter.get('/logout',logoutController)
 
 export {
     userRouter
