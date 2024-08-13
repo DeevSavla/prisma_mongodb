5d2d5343e8ee16eb1d24f7ee764973761fb87bca
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2024-08-13 17:59:09 +0530

    add middleware

---
+++ middleware/isLoggedIn.js
@@ -0,0 +1,28 @@
+import {prisma} from '../prisma/index.js'
+import jwt from 'jsonwebtoken'
+
+//every middleware needs a next and it is only for verification
+export const isLoggedIn = async(req,res,next)=>{
+    try {
+        //req.cookie is a method that helps to access the cookie and then .(the name of the cookie)
+        const token = req.cookie.token
+
+        if(!token){
+            res.send('Please login')
+            throw new Error('You are not logged in') //send a response and close next (ideal case)
+        }
+
+        const decodedToken = jwt.verify(token,process.env.JWT_SECRET)
+
+        req.user = await prisma.user.findUnique({
+            where:{
+                id:decodedToken.userId
+            }
+        })
+
+        next() //proceed to other middlewares or controllers
+
+    } catch (error) {
+        throw new Error(error)
+    }
+}
